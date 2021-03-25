50eaecfeaaae27cad822c98fa2150a55faacf398
sub-c <halolockdown@yahoo.com>
2021-03-24 20:18:54 -0700

    started sandbox and model project for pinvoke branch

---
+++ AllegroDotNet.Models/AllegroColor.managed.cs
@@ -0,0 +1,10 @@
+﻿namespace AllegroDotNet.Models
+{
+    /// <summary>
+    /// A structure that describes a color in a device independent way.
+    /// </summary>
+    sealed partial class AllegroColor
+    {
+        internal NativeAllegroColor native;
+    }
+}
---
+++ AllegroDotNet.Models/AllegroColor.native.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AllegroDotNet.Models
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct NativeAllegroColor
+    {
+        float r, g, b, a;
+    }
+}
