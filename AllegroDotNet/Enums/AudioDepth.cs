72358cc3fafe5958b029fc0b556c193c3320ca90
Sub-C <halolockdown@yahoo.com>
2021-07-17 11:09:17 -0700

    added comments to file, audiodepth, constants

--- AllegroDotNet/Enums/AudioDepth.cs
+++ AllegroDotNet/Enums/AudioDepth.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 
 namespace SubC.AllegroDotNet.Enums
 {
@@ -9,14 +9,44 @@ namespace SubC.AllegroDotNet.Enums
     [Flags]
     public enum AudioDepth : int
     {
+        /// <summary>
+        /// 8-bit signed.
+        /// </summary>
         Int8 = 0x00,
+
+        /// <summary>
+        /// 16-bit signed.
+        /// </summary>
         Int16 = 0x01,
+
+        /// <summary>
+        /// 24-bit signed.
+        /// </summary>
         Int24 = 0x02,
+
+        /// <summary>
+        /// 32-bit floating point.
+        /// </summary>
         Float32 = 0x03,
+
+        /// <summary>
+        /// Bitwise OR this to turn audio depth unsigned.
+        /// </summary>
         Unsigned = 0x08,
 
+        /// <summary>
+        /// 8-bit unsigned.
+        /// </summary>
         UInt8 = Int8 | Unsigned,
+
+        /// <summary>
+        /// 16-bit unsigned.
+        /// </summary>
         UInt16 = Int16 | Unsigned,
+
+        /// <summary>
+        /// 24-bit unsigned.
+        /// </summary>
         UInt24 = Int24 | Unsigned
     }
 }
