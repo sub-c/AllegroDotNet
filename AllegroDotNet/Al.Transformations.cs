eee21ad6d1dddd006a3899a408c02e61b4d60a0f
Sub-C <halolockdown@yahoo.com>
2021-07-24 16:49:53 -0700

    added last missing documentation

--- AllegroDotNet/Al.Transformations.cs
+++ AllegroDotNet/Al.Transformations.cs
@@ -1,10 +1,35 @@
-﻿namespace SubC.AllegroDotNet
+﻿using SubC.AllegroDotNet.Models;
+using SubC.AllegroDotNet.Native.Libraries;
+
+namespace SubC.AllegroDotNet
 {
     /// <summary>
     /// Allegro game library methods.
     /// </summary>
     public static partial class Al
     {
+        /// <summary>
+        /// Sets the projection transformation to be used for the drawing operations on the target bitmap (each bitmap
+        /// maintains its own projection transformation). Every drawing operation after this call will be transformed
+        /// using this transformation.
+        /// <para>
+        /// This function does nothing if there is no target bitmap. This function also does nothing if the bitmap is a
+        /// memory bitmap (i.e. memory bitmaps always use an orthographic transform like the snippet above). Note that
+        /// the projection transform will be reset to default if a video bitmap is converted to a memory bitmap.
+        /// Additionally, if the bitmap in question is the backbuffer, it’s projection transformation will be reset to
+        /// default if it is resized. Lastly, when you draw a memory bitmap to a video bitmap with a custom projection
+        /// transform, this transformation will be ignored (i.e. it’ll be as if the projection transform of the target
+        /// bitmap was temporarily reset to default).
+        /// </para>
+        /// <para>
+        /// The parameter is passed by reference as an optimization to avoid the overhead of stack copying. The
+        /// reference will not be stored in the Allegro library so it is safe to pass references to local variables.
+        /// </para>
+        /// </summary>
+        /// <param name="trans">The transformation instance.</param>
+        public static void UseProjectionTransform(AllegroTransform trans) =>
+            AllegroLibrary.AlUseProjectionTransform(ref trans.Native);
+
         #region P/Invokes
         //[DllImport(AlConstants.AllegroMonolithDllFilenameWindows)]
         //private static extern void al_copy_transform(ref NativeTransform dest, ref NativeTransform src);
--- AllegroDotNet/Enums/RenderFunction.cs
+++ AllegroDotNet/Enums/RenderFunction.cs
@@ -1,14 +1,48 @@
 ﻿namespace SubC.AllegroDotNet.Enums
 {
+    /// <summary>
+    /// Render function.
+    /// </summary>
     public enum RenderFunction : int
     {
+        /// <summary>
+        /// Never.
+        /// </summary>
         Never,
+
+        /// <summary>
+        /// Always.
+        /// </summary>
         Always,
+
+        /// <summary>
+        /// Less.
+        /// </summary>
         Less,
+
+        /// <summary>
+        /// Equal.
+        /// </summary>
         Equal,
+
+        /// <summary>
+        /// Less equal.
+        /// </summary>
         LessEqual,
+
+        /// <summary>
+        /// Greater.
+        /// </summary>
         Greater,
+
+        /// <summary>
+        /// Not equal.
+        /// </summary>
         NotEqual,
+
+        /// <summary>
+        /// Greater equal.
+        /// </summary>
         GreaterEqual
     }
 }
